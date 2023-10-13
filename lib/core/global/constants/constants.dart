4cfc1f44c7570603dc45b6e9cb175a92e94cb974
Ahmed Elkenawy <ahmed.elkenawy95@gmail.com>
2023-10-14 00:21:26 +0300

    add cart api

--- lib/core/global/constants/constants.dart
+++ lib/core/global/constants/constants.dart
@@ -4,5 +4,7 @@ class Constants {
   // ignore: non_constant_identifier_names
   static const ProjectName = "clothes store";
   static const ApiUrl = "https://fakestoreapi.com/products";
+  static const category = "/category/";
+
   static const to_cart = "Cart";
 }
--- lib/core/server/product-apis.dart
+++ lib/core/server/product-apis.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: file_names
 
 import 'dart:convert';
@@ -7,6 +6,7 @@ import 'package:http/http.dart' as http;
 
 import '../../model/product-model.dart';
 import '../global/constants/constants.dart';
+
 class ProductApis {
   static Future<List<Product>> getData() async {
     String url = Constants.ApiUrl;
@@ -17,6 +17,25 @@ class ProductApis {
       List data = jsonDecode(jsonData.body);
       List<Product> productList = [];
 
+      for (var a in data) {
+        Product allProductObject = Product.fromJson(a);
+        productList.add(allProductObject);
+      }
+      return productList;
+    } else {
+      throw Exception("Error");
+    }
+  }
+
+  static Future<List<Product>> getProductWithCategory(String category) async {
+    String url = Constants.ApiUrl + Constants.category + category;
+    print(1);
+    var jsonData = await http.get(Uri.parse(url));
+    print(2);
+    if (jsonData.statusCode == 200) {
+      List data = jsonDecode(jsonData.body);
+      List<Product> productList = [];
+    print(3);
       for (var a in data) {
         Product allProductObject = Product.fromJson(a);
         productList.add(allProductObject);
